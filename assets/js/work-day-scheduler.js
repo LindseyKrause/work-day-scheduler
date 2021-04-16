2f97c82b7a266677c555db5f493a322b9a28ecf0
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-15 21:13:47 -0700

    Added date and time stamp

--- assets/js/work-day-scheduler.js
+++ assets/js/work-day-scheduler.js
@@ -1,5 +1,7 @@
 var currentDate = document.querySelector("#currentDate");
-var now = moment();
+var now = moment().format('llll');
 console.log("now");
 console.log(now);
 currentDateEl = document.createElement("h3");
+currentDateEl.innerText = now;
+currentDate.appendChild(currentDateEl);
