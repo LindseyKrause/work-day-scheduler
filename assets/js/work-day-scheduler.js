d9cc77419256c7e892119e5fdc82752d4f4b4a42
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-16 21:41:10 -0700

    added time slots, time labels, buttons

--- assets/js/work-day-scheduler.js
+++ assets/js/work-day-scheduler.js
@@ -1,7 +1,28 @@
-var currentDate = document.querySelector("#currentDate");
-var now = moment().format('llll');
-console.log("now");
-console.log(now);
-currentDateEl = document.createElement("h3");
-currentDateEl.innerText = now;
-currentDate.appendChild(currentDateEl);
+$(document).ready(function () {
+
+    var currentDate = document.querySelector("#currentDate");
+    var now = moment().format('dddd, MMMM Do YYYY');
+    console.log("now");
+    console.log(now);
+    currentDateEl = document.createElement("h3");
+    currentDateEl.innerText = now;
+    currentDate.appendChild(currentDateEl);
+
+
+
+    //color code blocks to indicate past, present, future if scheduletime = now then make it magenta, if it's in the future make it yellow, if it's in the past make it teal.
+
+
+    //event listener click on save then save in local storage and make persist
+    $(".save-button").on('click', function () {
+        let time = $(this).attr("id");
+        let textValue = $(this).siblings("textarea").val();
+        console.log(time, textValue);
+        var saveTask = function () {
+            localStorage.setItem("time", JSON.stringify(time));
+            localStorage.setItem("textValue", JSON.stringify(textValue));
+
+        }
+    })
+
+})
